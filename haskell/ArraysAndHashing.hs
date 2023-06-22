a97f809450ac6bb7c8821edf3dd5fdf2b77d6144
andrii <25188+unorsk@users.noreply.github.com>
2023-06-22 16:18:24 +0200

    saving progress

--- haskell/ArraysAndHashing.hs
+++ haskell/ArraysAndHashing.hs
@@ -1,7 +1,15 @@
-module ArraysAndHashing where
-  
-import Data.HashSet (HashSet, empty)
+module ArraysAndHashing (containsDuplicate) where
+
+import Data.HashSet (empty, member, insert)
+import Data.Foldable (find)
+import Data.Maybe (fromMaybe)
+-- import Data.HashSet (HashSet, empty)
 
 containsDuplicate :: [Int] -> Bool
 containsDuplicate nums =
-  False
+  let m = empty in
+    case find (\a ->
+      member a m || (let _ = insert a m in False)
+      ) nums of
+      Just _ -> True
+      _ -> False
